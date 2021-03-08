d4d19fead7919f280c9ad7690a92cd96d20cba33
tianhaoAA <1334750953@qq.com>
2021-03-08 10:00:21 +0800

    使用element UI组件库

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui' //引入ElementUI
+import 'element-ui/lib/theme-chalk/index.css'; //引入ElementUI 的样式
 
-Vue.config.productionTip = false
 
+Vue.config.productionTip = false   // 阻止显示生产模式的消息
+Vue.use(ElementUI) //注册ElementUI
 new Vue({
   router,
   store,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+     <el-button type="primary" disabled>主要按钮</el-button>
+      <el-button type="info" disabled>信息按钮</el-button>
+      <el-button type="danger" disabled>危险按钮</el-button>
   </div>
 </template>
